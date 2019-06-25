800f572aa7b56bd852bfb873c330045acd4aa6b1
ttdp <tiantong.dev@gmail.com>
2019-06-25 16:19:41 +0800

    Add tab bar hide animation

--- Transitions/ScaleUpTransition/PhotoDetailPushTransition.swift
+++ Transitions/ScaleUpTransition/PhotoDetailPushTransition.swift
@@ -97,6 +97,11 @@ class PhotoDetailPushTransition: NSObject, UIViewControllerAnimatedTransitioning
             self.fromDelegate.transitionDidEnd()
         }
         
+        guard let tabBarController = transitionContext.viewController(forKey: .from)?.tabBarController as? TabBarController else {
+            return
+        }
+        tabBarController.setTabBar(hidden: true, animated: true, alongside: animator)
+        
         // Here, we kick off the animation.
         animator.startAnimation()
     }
--- Transitions/TabBarController.swift
+++ Transitions/TabBarController.swift
@@ -14,6 +14,12 @@ import UIKit
  **/
 class TabBarController: UITabBarController {
     
+    fileprivate var isTabBarHidden: Bool = false
+    
+    fileprivate var isTabBarOffScreen: Bool {
+        return !tabBar.frame.intersects(view.frame)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,7 +41,61 @@ class TabBarController: UITabBarController {
     // MARK: Animator
     
     func setTabBar(hidden: Bool, animated: Bool = true, alongside animator: UIViewPropertyAnimator? = nil) {
+        guard isTabBarHidden != hidden else { return }
+        isTabBarHidden = hidden
         
+        let offsetY = hidden ? tabBar.frame.height : -tabBar.frame.height
+        let endFrame = tabBar.frame.offsetBy(dx: 0, dy: offsetY)
+        let vc = selectedViewController
+        
+        var newInsets: UIEdgeInsets? = vc?.additionalSafeAreaInsets
+        let originalInsets = newInsets
+        newInsets?.bottom -= offsetY
+        
+        func set(childViewController cvc: UIViewController?, additionalSafeArea: UIEdgeInsets) {
+            cvc?.additionalSafeAreaInsets = additionalSafeAreaInsets
+            cvc?.view.setNeedsLayout()
+        }
+        
+        if hidden, let insets = newInsets {
+            set(childViewController: vc, additionalSafeArea: insets)
+        }
+        
+        guard animated else {
+            tabBar.frame = endFrame
+            tabBar.isHidden = isTabBarHidden
+            return
+        }
+        
+        if tabBar.isHidden, !hidden {
+            tabBar.isHidden = false
+        }
+        
+        weak var tabBarRef = self.tabBar
+        if let animator = animator {
+            animator.addAnimations {
+                tabBarRef?.frame = endFrame
+            }
+            
+            animator.addCompletion { position in
+                let insets = (position == .end) ? newInsets : originalInsets
+                if !hidden, let insets = insets {
+                    set(childViewController: vc, additionalSafeArea: insets)
+                }
+                if position == .end {
+                    tabBarRef?.isHidden = hidden
+                }
+            }
+        } else {
+            UIView.animate(withDuration: 0.3, animations: {
+                tabBarRef?.frame = endFrame
+            }) { didFinish in
+                if !hidden, didFinish, let insets = newInsets {
+                    set(childViewController: vc, additionalSafeArea: insets)
+                }
+                tabBarRef?.isHidden = hidden
+            }
+        }
     }
     
 }
